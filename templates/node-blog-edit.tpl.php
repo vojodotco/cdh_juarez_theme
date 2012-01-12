d4b1ba4873cfc70f3c69d3e69e56021db090e96e
Rahul Bhargava <rahulb@media.mit.edu>
2012-01-12 16:44:39 -0500

    hide attachments too

--- templates/node-blog-edit.tpl.php
+++ templates/node-blog-edit.tpl.php
@@ -8,6 +8,7 @@
 // remove some other buttons
 unset($form['buttons']['preview']);
 unset($form['locations']);
+unset($form['attachments']);
 
 // include the libraries for geocoding from the single address text input field
 drupal_set_html_head('<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>');
@@ -44,7 +45,6 @@ drupal_add_js(drupal_get_path('theme', 'cdh_juarez') . '/js/location-autocomplet
                 value="<?php print $form['#parameters'][2]->locations[0]['longitude'] ?>" 
                 name="locations[0][locpick][user_longitude]">            
         </div>
-        
 
         <?php print drupal_render($form['language']); ?>
     
